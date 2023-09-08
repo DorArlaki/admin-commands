30294d91815fb12fd42344f7a54221884bbbc4ae
A. Dor <107101354+DorArlaki@users.noreply.github.com>
2023-09-09 00:07:41 +0300

    Create CleanUserTemporaryFiles.ps1

---
+++ Powershell Scripts/PowerShell_Scripts/CleanUserTemporaryFiles.ps1
@@ -0,0 +1,26 @@
+# Clean the user's Temp folder
+$TempPath = [System.IO.Path]::GetTempPath()
+
+# Check if the Temp folder exists
+if (Test-Path -Path $TempPath -PathType Container) {
+    try {
+        # Get a list of files to be deleted (excluding those currently in use)
+        $FilesToDelete = Get-ChildItem -Path $TempPath -File | Where-Object { !$_.IsReadOnly }
+
+        # Delete files in the Temp folder
+        foreach ($File in $FilesToDelete) {
+            try {
+                Remove-Item -Path $File.FullName -Force -ErrorAction Stop
+            } catch {
+                # If an error occurs (e.g., file in use), silently skip the file
+                continue
+            }
+        }
+
+        # Optionally, you can display a message indicating cleanup completion
+        # Write-Host "Cleanup completed for $($env:USERNAME)."
+
+    } catch {
+        # Silently handle any unexpected errors
+    }
+}
