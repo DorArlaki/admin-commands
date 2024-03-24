204ca0343c4422846da9dfee2eb4c4d3532fe066
A. Dor <107101354+DorArlaki@users.noreply.github.com>
2024-03-24 16:35:24 +0200

    Update Export-InactiveUserAccounts.ps1

--- Powershell Scripts/PowerShell_Scripts/Export-InactiveUserAccounts/Export-InactiveUserAccounts.ps1
+++ Powershell Scripts/PowerShell_Scripts/Export-InactiveUserAccounts/Export-InactiveUserAccounts.ps1
@@ -1,5 +1,5 @@
 # Define the number of days to consider an account as inactive
-$InactiveDaysThreshold = 1
+$InactiveDaysThreshold = 30  # Adjust as needed
 
 # Get the current date
 $CurrentDate = Get-Date
@@ -9,11 +9,11 @@ $ThresholdDate = $CurrentDate.AddDays(-$InactiveDaysThreshold)
 
 # Query Active Directory for user accounts in the "soreq.local" domain
 $UserAccounts = Get-ADUser -Filter {Enabled -eq $true} -Properties Name, LastLogonDate | Where-Object {
-    $_.LastLogonDate -ne $null -and $_.LastLogonDate -lt $ThresholdDate
+    $_.LastLogonDate -eq $null -or $_.LastLogonDate -lt $ThresholdDate
 }
 
 # Specify the directory path where you want to save the CSV file
-$CSVDirectoryPath = "C:\Share\GPO\Scripts"
+$CSVDirectoryPath = "C:\Users\dor\Desktop\Dor\users"
 
 # Specify the name of the CSV file
 $CSVFileName = "InactiveUserAccounts.csv"
@@ -27,7 +27,7 @@ $InactiveUsers = @()
 # Loop through user accounts and identify inactive users
 foreach ($User in $UserAccounts) {
     $LastLogonDate = $User.LastLogonDate
-    $DaysSinceLastLogon = ($CurrentDate - $LastLogonDate).Days
+    $DaysSinceLastLogon = if ($LastLogonDate) { ($CurrentDate - $LastLogonDate).Days } else { "Never logged in" }
 
     $UserData = [PSCustomObject]@{
         Name = $User.Name
@@ -37,9 +37,9 @@ foreach ($User in $UserAccounts) {
     $InactiveUsers += $UserData
 }
 
-# Export the list of inactive users to a CSV file
+# Export the list of inactive users to a CSV file with UTF-8 encoding
 if ($InactiveUsers.Count -gt 0) {
-    $InactiveUsers | Export-Csv -Path $CSVFilePath -NoTypeInformation
+    $InactiveUsers | Export-Csv -Path $CSVFilePath -NoTypeInformation -Encoding UTF8
 
     Write-Host "Inactive user accounts (not logged in for $InactiveDaysThreshold days or more) have been saved to $CSVFilePath."
 } else {
